7f3481ca4dc3969841b0caba5f08dcefcf68b343
Adrian Sudbury <sudburya@gmail.com>
2022-10-12 11:07:54 +0100

    Initial commit

---
+++ test/aws-stepfunction-holiday-booking-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsStepfunctionHolidayBookingCdk from '../lib/aws-stepfunction-holiday-booking-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-stepfunction-holiday-booking-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsStepfunctionHolidayBookingCdk.AwsStepfunctionHolidayBookingCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
